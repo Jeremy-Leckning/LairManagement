b01752099ef4f676df4f9a8713ac35236e00aace
Jeremy Leckning <jlec0001@student.monash.edu>
2019-04-14 23:29:17 +1000

    test

--- src/Lair.java
+++ src/Lair.java
@@ -7,7 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-// test
 
 public class Lair {
 	private LairLocation[] LairLocations;
